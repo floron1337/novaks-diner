b3d6398f2894f6f5111a920b40c4d9b12f22ebe9
Florin Veniș <142159449+floron1337@users.noreply.github.com>
2025-07-14 15:14:29 +0300

    updated portfolio link

--- components/layout/navbar/Disclaimer.tsx
+++ components/layout/navbar/Disclaimer.tsx
@@ -5,7 +5,7 @@ const Disclaimer = () => {
     return (
         <div className='w-full bg-black text-white flex flex-col justify-center items-center'>
             <h4 className='text-center'>THIS WEBSITE IS AN EXAMPLE TO SERVE THE PERSONAL PORTFOLIO OF FLORIN VENIS. ALL BUSINESS DETAILS ARE FICTIONAL.</h4>
-            <Link href="https://www.florinvenis.com/" target="_blank" className='flex flex-row gap-2 py-4 items-center hover:gap-8 ease'>
+            <Link href="https://florinvenis.vercel.app/" target="_blank" className='flex flex-row gap-2 py-4 items-center hover:gap-8 ease'>
                 <div className='flex flex-row gap-2'>
                     <img src='/img/logo_nobg.png' alt="" width={60} height={60} />
                     <div className='flex flex-col text-white justify-center'>
